129a6bc6567946249e2421fa80df0989eb4edfec
sanashajiya <ssanashajiya@gmail.com>
2025-05-19 15:25:37 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+// import 'firebase_options.dart'; // Uncomment if using FlutterFire CLI
+
+Future main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   // options: DefaultFirebaseOptions.currentPlatform, // Optional
+  // );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Auth',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Text("Firebase Auth"),
+      ),
+    );
+  }
+}
+
+// class MainPage extends StatelessWidget {
+//   const MainPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return StreamBuilder<User?>(
+//       stream: FirebaseAuth.instance.authStateChanges(),
+//       builder: (context, snapshot) {
+//         if (snapshot.connectionState == ConnectionState.waiting) {
+//           return const Center(child: CircularProgressIndicator());
+//         } else if (snapshot.hasError) {
+//           return Center(child: Text("Something went wrong"));
+//         }
+//         else if (snapshot.hasData) {
+//           return const HomePage();
+//         } else {
+//           return const AuthPage();
+//         }
+//       },
+//     );
+//   }
+// }
